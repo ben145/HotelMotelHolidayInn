ae4d13cddbd36c7c428dd1f6b2fdcbc2091d8f3c
Mia <ekimmichmitchell@ithaca.edu>
2018-03-23 13:11:21 -0400

    test

---
+++ src/test/java/edu/ithaca/bhamula1/hotel/RoomTest.java
@@ -0,0 +1,28 @@
+package edu.ithaca.bhamula1.hotel;
+
+/**
+ * Created by Mia on 3/22/2018.
+ */
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RoomTest {
+
+
+    @Test
+    void roomNumberTest(){
+        assertEquals(true,false);
+
+
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void roomDetailsTest(){
+
+    }
+
+
+
+}
