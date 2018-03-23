abe3c778c2846be4b184f5966aff5878d60ffdee
bkeith <bnkeith.20@gmail.com>
2018-03-22 22:27:46 -0400

    made empty check in check out tests

---
+++ src/test/java/edu/ithaca/bhamula1/hotel/HotelTest.java
@@ -0,0 +1,17 @@
+package edu.ithaca.bhamula1.hotel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class checkInOutTest {
+
+    @org.junit.jupiter.api.Test
+    void checkInTest(){
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkOutTest(){
+
+    }
+
+}
