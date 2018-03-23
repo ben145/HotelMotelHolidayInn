b9e905998c49aebef7467861748d6e6497e33010
bkeith <bnkeith.20@gmail.com>
2018-03-23 09:15:39 -0400

    fix home

--- src/main/java/edu/ithaca/bhamula1/hotel/Hotel.java
+++ src/main/java/edu/ithaca/bhamula1/hotel/Hotel.java
@@ -4,12 +4,4 @@ package edu.ithaca.bhamula1.hotel;
  * Created by Ben on 3/22/2018.
  */
 public class Hotel {
-
-    public void checkIn(Customer cust, Room room){
-        //find customer and make boolean true
-    }
-
-    public void checkOut(Customer cust, Room room){
-        //find customer and make boolean false
-    }
 }
