593db3e65ee647d9d761477c9e1e2cfce68f835e
bkeith <bnkeith.20@gmail.com>
2018-03-24 16:34:14 -0400

    Set up for checkin

--- src/main/java/edu/ithaca/bhamula1/hotel/Hotel.java
+++ src/main/java/edu/ithaca/bhamula1/hotel/Hotel.java
@@ -1,7 +1,32 @@
 package edu.ithaca.bhamula1.hotel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Ben on 3/22/2018.
  */
 public class Hotel {
+    private List<Room> rooms;
+    private List<Customer> customers;
+
+    public Hotel(){
+        rooms = new ArrayList<>();
+        customers = new ArrayList<>();
+    }
+
+    public void setupTestHotel(){
+        for(int i = 0; i<4; i++) {
+            rooms.add(new Room(i));
+        }
+        customers.add(new Customer("Brad", "Keith"));
+    }
+
+    public void checkIn(int roomNumber, Customer customer){
+        //find room
+        //check room is reserved
+        //find customer check matches stored room number
+        //set customer as checked in
+        //set room to be checked in
+    }
 }
--- src/main/java/edu/ithaca/bhamula1/hotel/Room.java
+++ src/main/java/edu/ithaca/bhamula1/hotel/Room.java
@@ -4,4 +4,13 @@ package edu.ithaca.bhamula1.hotel;
  * Created by Ben on 3/22/2018.
  */
 public class Room {
+    private boolean isReserved;
+    private boolean checkedIn;
+    private int number;
+
+    public Room(int num){
+        number = num;
+        isReserved = false;
+        checkedIn = false;
+    }
 }
