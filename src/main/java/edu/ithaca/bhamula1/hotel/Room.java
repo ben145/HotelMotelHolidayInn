ec8601e91e1cfe83dbb882e58d0ec2e44c721f9e
Mia <ekimmichmitchell@ithaca.edu>
2018-03-25 19:25:37 -0400

    room tests for getters, constructor, and setters

--- src/main/java/edu/ithaca/bhamula1/hotel/Room.java
+++ src/main/java/edu/ithaca/bhamula1/hotel/Room.java
@@ -4,4 +4,8 @@ package edu.ithaca.bhamula1.hotel;
  * Created by Ben on 3/22/2018.
  */
 public class Room {
+
+
+
+
 }
--- src/test/java/edu/ithaca/bhamula1/hotel/RoomTest.java
+++ src/test/java/edu/ithaca/bhamula1/hotel/RoomTest.java
@@ -1,5 +1,6 @@
 package edu.ithaca.bhamula1.hotel;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -9,16 +10,53 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 
+
 public class RoomTest {
 
-    @Test
-    void roomNumberTest(){
-        assertEquals(true,false);
 
+
+    //available, room number,price, number of beds,  type of bed,  extras, view
+
+    @Test
+    void setRoomTest() {
+        //available, room number,price, type of bed,  extras, view
+        Room exampleRoom = new Room();
+        exampleRoom.setIfAvailable(true);
+        exampleRoom.setRoomNumber(12);
+        exampleRoom.setRoomPrice(500);
+        exampleRoom.setBedCount(1);
+        exampleRoom.setBedType("Queen");
+        exampleRoom.addAmenities("mini bar");
+        exampleRoom.setIfView(true);
+    }
+
+
+    @Test
+    void getRoomTest() {
+        Room exampleRoom = new Room(true, 212, 500, 1, "queen", "mini bar", true);
+        Assert.assertEquals(true, exampleRoom.getIfAvailable());
+        Assert.assertEquals(12, exampleRoom.getRoomNumber());
+        Assert.assertEquals(1, exampleRoom.getBedCount);
+        Assert.assertEquals(500, exampleRoom.getRoomPrice());
+        Assert.assertEquals("", exampleRoom.getBedType());
+        Assert.assertEquals(true, exampleRoom.getIfView());
+        Assert.assertEquals("", exampleRoom.getAmenities());
     }
 
 
 
+    @Test
+    void RoomConstructorTest() {
+        //available, room number,price, number of beds,  type of bed,  extras, view
+        Room exampleRoom = new Room(true, 212, 500, 1, "queen", "mini bar", true);
+        Assert.assertEquals(true, exampleRoom.getIfAvailable());
+        Assert.assertEquals(12, exampleRoom.getRoomNumber());
+        Assert.assertEquals(1, exampleRoom.getBedCount);
+        Assert.assertEquals(500, exampleRoom.getRoomPrice());
+        Assert.assertEquals("", exampleRoom.getBedType());
+        Assert.assertEquals("", exampleRoom.getAmenities());
+        Assert.assertEquals(true, exampleRoom.getIfView());
+    }
 
 
 }
