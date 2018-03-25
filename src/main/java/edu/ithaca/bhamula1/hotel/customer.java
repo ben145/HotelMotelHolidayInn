a7d1bd5d9dd5f7010010dbd6b53984c8b2f257c7
Ben Hamula <bhamula1@ithaca.edu>
2018-03-25 19:28:02 -0400

    beginnings of customer class

---
+++ src/main/java/edu/ithaca/bhamula1/hotel/customer.java
@@ -0,0 +1,14 @@
+package edu.ithaca.bhamula1.hotel;
+
+/**
+ * Created by Ben on 3/25/2018.
+ */
+public interface customer {
+
+    public String makeName();
+    public String makeID();
+    public String getName();
+    public String getId();
+    public void login();
+
+}
---
+++ src/main/java/edu/ithaca/bhamula1/hotel/customerImpl.java
@@ -0,0 +1,78 @@
+package edu.ithaca.bhamula1.hotel;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by Ben on 3/25/2018.
+ */
+public class customerImpl implements customer {
+
+    private String name;
+    private String id;
+    private String fName;
+    private String lName;
+
+    customerImpl(String nameIn, String idIn) {
+        name = nameIn;
+        id = idIn;
+    }
+
+    customerImpl(){
+        name = makeName();
+        id = makeID();
+    }
+
+    @Override
+    public String makeName() {
+        String firstName, lastName;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your first name");
+        firstName = scan.next();
+        fName = firstName.substring(0,1).toUpperCase() + firstName.substring(1,firstName.length());
+        System.out.println("Please enter your last name");
+        lastName = scan.next();
+        lName = lastName.substring(0,1).toUpperCase() + lastName.substring(1,lastName.length());
+        return (fName +" "+ lName);
+    }
+
+    @Override
+    public String makeID() {
+        String flName = fName.substring(0,1) + lName.substring(0,1);
+        Random rand = new Random();
+        double num = rand.nextInt(100000);
+        int diff = 0;
+        String numStr = Double.toString(num);
+        String extraZeroes = "";
+        if (numStr.length() < 5) {
+            diff = 5 - numStr.length();
+            for (int i = 0; i < diff; i++)
+                extraZeroes = extraZeroes + "0";
+        }
+        return (flName + extraZeroes + numStr);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public void login() {
+        Scanner k = new Scanner(System.in);
+        String idIn;
+        System.out.println("Please enter your ID");
+        idIn = k.next();
+        String custID = "abc123";
+        if (idIn.equals(custID))
+            System.out.println("Log in successful");
+    }
+
+    public static void main(String[] args) {
+        customerImpl c = new customerImpl();
+        c.login();
+    }
+}
