bea486afad773a79b36e1c1a3d1ae9153bf65d00
Ben Hamula <bhamula1@ithaca.edu>
2018-03-26 09:24:23 -0400

    added few comments

--- src/main/java/edu/ithaca/bhamula1/hotel/customerImpl.java
+++ src/main/java/edu/ithaca/bhamula1/hotel/customerImpl.java
@@ -27,11 +27,15 @@ public class customerImpl implements customer {
     public String makeName() {
         String firstName, lastName;
         Scanner scan = new Scanner(System.in);
+        // getting first name and storing in firstName
         System.out.println("Please enter your first name");
         firstName = scan.next();
+        // reformatting so firstName has capital first letter, lowercase otherwise
         fName = firstName.substring(0,1).toUpperCase() + firstName.substring(1,firstName.length());
+        // getting last name and storing in lastName
         System.out.println("Please enter your last name");
         lastName = scan.next();
+        // reformatting so lastName has capital first letter, lowercase otherwise
         lName = lastName.substring(0,1).toUpperCase() + lastName.substring(1,lastName.length());
         return (fName +" "+ lName);
     }
@@ -41,12 +45,7 @@ public class customerImpl implements customer {
         String flName = fName.substring(0,1) + lName.substring(0,1);
         Random rand = new Random();
         double num = rand.nextInt(100000);
-        int diff = 0;
-        String numStr = Double.toString(num);
-        String extraZeroes = "";
-        if (numStr.length() < 5) {
-            diff = 5 - numStr.length();
-            for (int i = 0; i < diff; i++)
+        int diff = 0; diff; i++)
                 extraZeroes = extraZeroes + "0";
         }
         return (flName + extraZeroes + numStr);
