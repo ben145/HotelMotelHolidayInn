a782e0a3e85f531d45b30ca1f92a45f4e7453ea9
Mia <ekimmichmitchell@ithaca.edu>
2018-03-28 15:20:21 -0400

    added main class

---
+++ src/main/java/edu/ithaca/bhamula1/hotel/Main.java
@@ -0,0 +1,8 @@
+package edu.ithaca.bhamula1.hotel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
