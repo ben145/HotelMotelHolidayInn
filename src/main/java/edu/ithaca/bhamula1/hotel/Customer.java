c52f0fcf0b2a7298d976cac2b1ad11eda4f91529
Ben Hamula <bhamula1@ithaca.edu>
2018-03-28 16:23:52 -0400

    fixed customer to Customer in HotelTest; improved login experience

--- src/main/java/edu/ithaca/bhamula1/hotel/Customer.java
+++ src/main/java/edu/ithaca/bhamula1/hotel/Customer.java
@@ -34,31 +34,34 @@ public class Customer {
         String firstName, lastName;
         Scanner scan = new Scanner(System.in);
         // getting first name and storing in firstName
-        System.out.println("Please enter your first name");
+        System.out.print("Please enter your first name: ");
         firstName = scan.next();
         // reformatting so firstName has capital first letter, lowercase otherwise
         fName = firstName.substring(0,1).toUpperCase() + firstName.substring(1,firstName.length());
         // getting last name and storing in lastName
-        System.out.println("Please enter your last name");
+        System.out.print("Please enter your last name: ");
         lastName = scan.next();
         // reformatting so lastName has capital first letter, otherwise lowercase
         lName = lastName.substring(0,1).toUpperCase() + lastName.substring(1,lastName.length());
+        System.out.println("Welcome, " + fName + " " + lName);
         return (fName +" "+ lName);
     }
 
     public String makeID() {
         String flName = fName.substring(0,1) + lName.substring(0,1);
         Random rand = new Random();
-        double num = rand.nextInt(100000);
+        int num = rand.nextInt(100000);
         int diff = 0;
-        String numStr = Double.toString(num);
+        String numStr = Integer.toString(num);
         String extraZeroes = "";
         if (numStr.length() < 5) {
             diff = 5 - numStr.length();
             for (int i = 0; i < diff; i++)
                 extraZeroes = extraZeroes + "0";
         }
-        return (flName + extraZeroes + numStr);
+        String yourId = (flName + extraZeroes + numStr);
+        System.out.println("Your login ID is " + yourId);
+        return yourId;
     }
 
     public String getName() {
@@ -74,13 +77,16 @@ public class Customer {
         boolean successful = false;
         String idIn;
         String option;
-        System.out.println("Please enter your ID");
+        // getting id input attempt from user
+        System.out.print("Please enter your ID: ");
         idIn = scan.next();
+        // sets idIn to itself, making first two characters (letters) uppercase
+        //      so user doesn't need to type in initials as uppercase
+        idIn = idIn.substring(0,2).toUpperCase() + idIn.substring(2);
         if (idIn.equals(getId())) {
-            System.out.println("Log in successful");
+            System.out.println("\nLog in successful");
             isLoggedIn = true;
-        }
-        else {
+        } else {
             while (!successful) {
                 System.out.println("Incorrect login information. Please try again, or enter [D]one to exit.");
                 option = scan.next();
@@ -91,8 +97,7 @@ public class Customer {
                     System.out.println("Log in successful");
                     isLoggedIn = true;
                     successful = true;
-                } else
-                    System.out.println("Input undefined. Please try again.");
+                }
             }
         }
     }
