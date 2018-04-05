bcdc095b406a7243a0e4a9a9d49b098285399a83
bkeith <bnkeith.20@gmail.com>
2018-04-05 10:33:59 -0400

    test back in sorry

--- src/test/java/edu/ithaca/bhamula1/hotel/EmployeeIMPLTest.java
+++ src/test/java/edu/ithaca/bhamula1/hotel/EmployeeIMPLTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeIMPLTest {
-/*
+
     @Test
     void constructorTest(){
         fail("Hey");
     }
-    */
+
 
 }
