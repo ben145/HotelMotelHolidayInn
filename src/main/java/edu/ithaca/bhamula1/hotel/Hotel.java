adc194184cefa61b0bd196886afd918b2ac1bdad
mooooood <dfulle17@gmail.com>
2018-04-15 17:46:29 -0400

    hotel edit may be overwritten by merges

--- src/main/java/edu/ithaca/bhamula1/hotel/Hotel.java
+++ src/main/java/edu/ithaca/bhamula1/hotel/Hotel.java
@@ -15,6 +15,7 @@ public class Hotel implements HotelInterface {
     private List<CustomerInterface> customers;
     private List<EmployeeIMPL> employees;
     private List<InventoryInterface> inventory;
+    private List<RequestsInterface> activeRequests;
 
     public Hotel(){
         //this was a hash map. Changed to a array list
