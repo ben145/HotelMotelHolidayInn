7dc4cab1ba74d22ff261162ed26bc6bdeaefda23
Ben Hamula <bhamula1@ithaca.edu>
2018-04-30 12:15:42 -0400

    stashing

--- src/main/java/edu/ithaca/bhamula1/hotel/RequestsInterface.java
+++ src/main/java/edu/ithaca/bhamula1/hotel/RequestsInterface.java
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-package edu.ithaca.bhamula1.hotel;
-
-/**
- * 
- */
-public interface RequestsInterface {
-    /*
-    //active request functions
-    String getActiveRequest();
-    int getRequestRoomNumber();
-    String getActiveRequestEmployeeId();
-    boolean requestActive();
-    void setActiveRequest(String newRequest);
-    void setRequestRoomNumber(int newRoomNumber);
-    void setActiveRequestEmployeeId(String newEmployeeId);
-    void activateRequest();
-    */
-
-    /*request functions*/
-
-    void loadRecs();
-
-    void viewRequests();
-
-    //employees can add possibly requests to the system
-    void addRequest(String employeeId);
-
-    //employees can remove a possible request from the system
-    void removeRequest(String employeeId);
-
-    //a customer checked in to a room can make a request
-    void makeRequest(int roomNumber);
-}
-=======
 package edu.ithaca.bhamula1.hotel;
 
 /**
@@ -66,4 +31,3 @@ public interface RequestsInterface {
     //a customer checked in to a room can make a request
     String makeRequest(int roomNumber);
 }
->>>>>>> d1ec8b5bc89be206144c520dedcda605cfb30265
