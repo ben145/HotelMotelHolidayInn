ead70ed02b9639184b4de7e4e003b0a6f9b6e9c1
Ben Hamula <bhamula1@ithaca.edu>
2018-05-02 08:48:31 -0400

    request accept and fulfill

--- src/main/java/edu/ithaca/bhamula1/hotel/Requests.java
+++ src/main/java/edu/ithaca/bhamula1/hotel/Requests.java
@@ -251,10 +251,14 @@ public class Requests implements RequestsInterface{
         while (!authorized) {
             System.out.print("Enter your employee ID: ");
             String idInput = scanner.nextLine();
-            // not sure if easy way to loop through all employees and check
-            // inputID against them for authorization, simple test for now - Ben
-            if (idInput.length() == 7)
-                authorized = true;
+            for (Employee e: Hotel.employees) {
+                if (e.getE_LogID().equals(idInput)) {
+                    authorized = true;
+                    System.out.println("\nAuthorization successful.\n");
+                }
+            }
+            if (!authorized)
+                System.out.println("\nEmployee ID not recognized - please try again.\n");
         }
         //request name
             System.out.println("Enter request to add: ");
@@ -278,7 +282,7 @@ public class Requests implements RequestsInterface{
         System.out.println("Request added to options list");
     }
 
-    //allows authorized staff to remove a request to the system
+    //allows authorized staff to remove a request from the system
     public void removeRequest(String employeeId){
     	//check authorization
         Scanner scanner = new Scanner(System.in);
@@ -286,10 +290,14 @@ public class Requests implements RequestsInterface{
         while (!authorized) {
             System.out.print("Enter your employee ID: ");
             String idInput = scanner.nextLine();
-            // not sure if easy way to loop through all employees and check
-            // inputID against them for authorization, simple test for now - Ben
-            if (idInput.length() == 7)
-                authorized = true;
+            for (Employee e: Hotel.employees) {
+                if (e.getE_LogID().equals(idInput)) {
+                    authorized = true;
+                    System.out.println("\nAuthorization successful.\n");
+                }
+            }
+            if (!authorized)
+                System.out.println("\nEmployee ID not recognized - please try again.\n");
         }
         //remove requests
         viewRequests();
@@ -329,4 +337,73 @@ public class Requests implements RequestsInterface{
         }
         return "";
     }
+
+    /**
+     * Finds first available request to fulfill (earliest among requests gets priority)
+     *  and that is then assigned to employee upon entering their ID
+     * @author Ben H.
+     * @post chomething sanges
+     */
+    public void acceptRequest() {
+        if (Hotel.activeRequests.size() > 0) {
+            int i = 0;
+            ActiveRequest actReq = Hotel.activeRequests.get(i);
+            while (actReq.requestActive()) {
+                i++;
+                actReq = Hotel.activeRequests.get(i);
+            }
+            Scanner scan = new Scanner(System.in);
+            boolean authorized = false;
+            String acceptID = "";
+            while (!authorized) {
+                System.out.print("Enter your employee ID: ");
+                String idInput = scan.nextLine();
+                for (Employee e: Hotel.employees) {
+                    if (e.getE_LogID().equals(idInput)) {
+                        if (e.getE_Available()) {
+                            authorized = true;
+                            acceptID = e.getE_LogID();
+                            e.setE_Available(false);
+                            System.out.println("\nAuthorization successful.\n");
+                        }
+                    }
+                }
+                if (!authorized)
+                    System.out.println("\nEmployee ID not recognized - please try again.\n");
+            }
+            System.out.println("\nYou have been assigned the next available request, "+actReq.getActiveRequest()+
+                    " for Room "+actReq.getRequestRoomNumber()+".");
+            // employee accepts request to fulfill, request is made activate, then removed as completed
+            actReq.setActiveRequestEmployeeId(acceptID);
+            boolean fulfilled = fulfillRequest(actReq);
+            if (fulfilled)
+                Hotel.activeRequests.remove(actReq);
+            else
+                System.out.println("Request has not been fulfilled. It remains to be completed.");
+        }
+        else
+            System.out.println("Currently there are no requests to be fulfilled.");
+    }
+
+    /**
+     * Attempt at fulfilling request is made, following acceptance of request by employee
+     * @author Ben H.
+     * @param actReq request to be completed
+     * @return true if request is successfully completed, false otherwise
+     */
+    public boolean fulfillRequest(ActiveRequest actReq) {
+        String requestItem = actReq.getActiveRequest();
+        for (Inventory inv : Hotel.inventory) {
+            if (inv.getItem().equals(requestItem)) {
+                if (inv.getQuantity() > 0) {
+                    actReq.activateRequest();
+                    // subtract 1 instance from current count
+                    inv.setQuantity(inv.getQuantity() - 1);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
