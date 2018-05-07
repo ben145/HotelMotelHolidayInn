e7c3affaf924e23e6b3c02ff8c68caff139c732f
Ben Hamula <bhamula1@ithaca.edu>
2018-05-07 13:37:26 -0400

    added Javadocs, minor reformatting

--- src/main/java/edu/ithaca/bhamula1/hotel/EmployeeUI.java
+++ src/main/java/edu/ithaca/bhamula1/hotel/EmployeeUI.java
@@ -10,7 +10,7 @@ import static edu.ithaca.bhamula1.hotel.Hotel.viewInventory;
 
 /**
  * Employee UI
- * @athor DMF
+ * @author DMF
  */
 
 public class EmployeeUI implements EmployeeUI_Interface{
@@ -19,42 +19,25 @@ public class EmployeeUI implements EmployeeUI_Interface{
     private HotelInterface hotel;
     private boolean employeeScreenActive;
 
-// added this to use requests - think the requests should be in Hotel as a list with room number associated?
     private Requests requests;
-    boolean invalid = true;
+    private boolean invalid = true;
 
-    public EmployeeUI(){
+    // Employee User Interface constructor
+    public EmployeeUI() {
         requests = new Requests();
         employeeScreenActive = true;
     }
 
-    /**
-     * sets Employee to this instance of login screen
-     * @param e
-     */
-    void setEmployee(EmployeeIMPL e){
-        this.employee = e;
-    }
+    public String getEmplTitle(){return employee.getE_Title();}
 
-    /**
-     * returns employee title - used for test
-     * @return
-     */
-    public String getEmplTitle(){
-        return employee.getE_Title();
-    }
+    public void setEmployee(EmployeeIMPL e){this.employee = e;}
 
-    /**
-     * All UI interaction below - Employee can make choice if logged in
-     * Takes in Hotel object oto get employee list and other data associated with the hotel
-     * @param h
-     */
-    public void uiInteraction(HotelInterface h){
+    public void uiInteraction(HotelInterface h) {
         this.hotel = h;
         System.out.println("------<>----Employee login page----<>------");
         employeeLogScreenUI();
-        while(employeeScreenActive){
-            if(!invalid){
+        while (employeeScreenActive) {
+            if (!invalid) {
                 System.out.println("\n@>--->---- Employee Menu Page ----<---<@");
 
                 Scanner scan = new Scanner(System.in);
@@ -66,103 +49,62 @@ public class EmployeeUI implements EmployeeUI_Interface{
                         "\t6) View Hotel Inventory\n" +
                         "\t7) LogOut\n"+"\nEnter Number Here -> ");
                 int option = scan.nextInt();
-                if(option == 1){
+                if (option == 1) {
                     System.out.println("\nPlease see open Customer Requests below:");
                     viewOpenRequests();
                     System.out.println("\n");
-                }else if(option == 2){
-                    if(!employee.getE_Available()){
+                }
+                else if (option == 2) {
+                    if (!employee.getE_Available()) {
                         System.out.println("You must complete the current request assigned to you before you can help another customer\n");
                         System.out.println("Please continue to work on the following request:\n");
                         checkEmpReq();
-
-                    }else {
+                    } else {
                         System.out.println("\nPlease enter customer request number to work on below:\n");
                         viewOpenRequests();
                         takeRequests(scan.nextInt());
                     }
 
-                }else if(option == 3){
-                    if(checkEmpReq()){
+                }
+                else if (option == 3) {
+                    if (checkEmpReq()) {
                         System.out.println("\nPlease enter customer request number you wish to complete below:\n");
                         completeRequest(scan.nextInt());
-                    }else{
-                        System.out.println("\nYou currently are not assigned to any customer requests\n");
                     }
+                    else
+                        System.out.println("\nYou currently are not assigned to any customer requests\n");
                 }
-                else if(option == 4){
+                else if (option == 4)
                     requests.addRequest();
-                }
-                else if(option == 5) {
+
+                else if (option == 5)
                     requests.removeRequest();
-                }
-                else if(option == 6){
+
+                else if (option == 6)
                     Hotel.viewInventory();
-                }else if(option==7){
+
+                else if (option == 7)
                     closeUI();
-                }
 
-
-            }else{
+            }
+            else
                 employeeScreenActive=false;
-            }
-
         }
     }
 
-    /**
-     * Before employee can chose options, valid input
-     * must be entered - this method checks for valid input
-     * and calls checkEmployeeLogIn to verify login data
-     */
-    @Override
-    public void employeeLogScreenUI() {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("\nEnter Employee LogIn ID:");
-        String eID = scan.nextLine();
-        System.out.print("\nEnter your password:");
-        String ePwd = scan.nextLine();
-
-        if(eID.isEmpty()||ePwd.isEmpty()){
-            System.out.println("\nInput cannot be blank, please try again\n");
-            employeeLogScreenUI();
-        }else{
-            checkEmployeeLogIn(eID,ePwd);
-            if(!invalid){
-                System.out.println("\n---> Employee Has Succesfully Logged In");
-                System.out.println("\n\n\tWelcome "+employee.getE_FirstName()+" "+
-                        employee.getE_LastName()+" - "+employee.getE_Title()+"\n\n\t\t\t\t<o> <o>\n\t\t\t\t   o\n\t\t\t\t \\___/\n\n ----<---<@ We value your service @>--->--");
-            }else{
-                System.out.println("\n** You have entered an invalid employee login ID/Password combination. **\n" +
-                        "\n\t\t*** Access Denied To Employee System ***\n\n\n ---<--<@   Main Menu   @>-->---\n");
-            }
-        }
-    }
-
-    /**
-     * Validates E Login and PWD based on user input
-     * compared to employee list
-     * if login is accurate, save employee state
-     * @param el
-     * @param epwd
-     * @author - DMF
-     */
     @Override
     public void checkEmployeeLogIn(String el, String epwd){
-
         int index = 0;
-        List<EmployeeIMPL>empls = hotel.getEList();
+        List<EmployeeIMPL> empls = hotel.getEList();
         Iterator iterator = hotel.getEList().iterator();
-        while(iterator.hasNext() && index!=hotel.getEList().size()){
-            //System.out.println(index + "  " +employees.size());
-            //System.out.println(employees.get(index).toString());
+        while (iterator.hasNext() && index != hotel.getEList().size()) {
             empls.get(index).checkE_LoginID(el);
-            if(empls.get(index).checkE_LoginID(el)
-                    && empls.get(index).checkE_PWD(epwd)){
+            if (empls.get(index).checkE_LoginID(el)
+                    && empls.get(index).checkE_PWD(epwd)) {
                 invalid = false;
                 // store employee values in UI
                 setEmployee(empls.get(index));
-                if(!employee.getE_LoggedIn()){
+                if (!employee.getE_LoggedIn()) {
                     employee.setE_LoggedIn(true);
                     hotel.saveEmplList();
                 }
@@ -171,33 +113,47 @@ public class EmployeeUI implements EmployeeUI_Interface{
         }
     }
 
-    /**
-     * Function displays active requests made for specific room numbers
-     * uses the static list of activeRequests in Hotel
-     * If no requests, displays a message that there are no active requests
-     * @author DMF
-     */
+    @Override
+    public void employeeLogScreenUI() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("\nEnter Employee LogIn ID: ");
+        String eID = scan.nextLine();
+        System.out.print("\nEnter your password: ");
+        String ePwd = scan.nextLine();
+
+        if( eID.isEmpty() || ePwd.isEmpty()) {
+            System.out.println("\nInput cannot be blank, please try again.\n");
+            employeeLogScreenUI();
+        }
+        else {
+            checkEmployeeLogIn(eID,ePwd);
+            if (!invalid) {
+                System.out.println("\n---> Employee Has Succesfully Logged In");
+                System.out.println("\n\n\tWelcome "+employee.getE_FirstName()+" "+
+                        employee.getE_LastName()+" - "+employee.getE_Title()+"\n\n\t\t\t\t<o> <o>\n\t\t\t\t   o\n\t\t\t\t \\___/\n\n ----<---<@ We value your service @>--->--");
+            }
+            else
+                System.out.println("\n** You have entered an invalid employee login ID/Password combination. **\n" +
+                        "\n\t\t*** Access Denied To Employee System ***\n\n\n ---<--<@   Main Menu   @>-->---\n");
+        }
+    }
+
     @Override
     public void viewOpenRequests() {
-
-        if(Hotel.activeRequests.size()>0) {
-
+        if (Hotel.activeRequests.size() > 0) {
             System.out.println("\n-- Current number of Active Guest Requests -->  "+ Hotel.activeRequests.size()+"\n");
-            for (int a = 0; a < activeRequests.size(); a++) {
+            for (int a = 0; a < activeRequests.size(); a++)
                 System.out.format("%-15s %15s %n","Active Request: " + (a+1) , "~ Room Number: " + activeRequests.get(a).roomNumber + "\t request:  " + activeRequests.get(a).request);
-            }
             System.out.println("\n---- End Active Guest Requests ----");
-        }else{
-            System.out.println("\n<-- There are no Active Guest Requests at this time -->\n");
         }
-
+        else
+            System.out.println("\n<-- There are no Active Guest Requests at this time -->\n");
     }
 
     @Override
     public void takeRequests(int req) {
-
-        for(int ar = 0; ar < activeRequests.size(); ar++){
-            if(ar+1 == req){
+        for (int ar = 0; ar < activeRequests.size(); ar++) {
+            if (ar+1 == req) {
                 activeRequests.get(ar).employeeId = employee.getE_LogID();
                 employee.setE_Available(false);
             }
@@ -205,9 +161,9 @@ public class EmployeeUI implements EmployeeUI_Interface{
     }
 
     @Override
-    public boolean checkEmpReq(){
-        for(int ar = 0; ar < activeRequests.size(); ar++){
-            if(employee.getE_LogID() == activeRequests.get(ar).employeeId){
+    public boolean checkEmpReq() {
+        for (int ar = 0; ar < activeRequests.size(); ar++) {
+            if (employee.getE_LogID().equals(activeRequests.get(ar).employeeId)) {
                 System.out.format("%-15s %15s %n","Your Assigned Request: " + (ar+1) , "~ Room Number: " + activeRequests.get(ar).roomNumber + "\t request:  " + activeRequests.get(ar).request);
                 return true;
             }
@@ -215,30 +171,20 @@ public class EmployeeUI implements EmployeeUI_Interface{
         return false;
     }
 
-
-
-
     @Override
     public void completeRequest(int req) {
-        for(int ar = 0; ar < activeRequests.size(); ar++){
-            if(ar+1 == req && activeRequests.get(ar).employeeId == employee.getE_LogID()){
+        for (int ar = 0; ar < activeRequests.size(); ar++) {
+            if (ar+1 == req && activeRequests.get(ar).employeeId.equals(employee.getE_LogID())) {
                 System.out.format("%-15s %15s %n","Request: " + (ar+1) , "~ Room Number: " + activeRequests.get(ar).roomNumber + "\t request:  " + activeRequests.get(ar).request);
                 employee.setE_Available(true);
                 activeRequests.remove(ar);
                 System.out.println("Has been fulfilled");
-            }else{
-                System.out.println("You have entered an invalid request number, try this again, please.\n");
             }
+            else
+                System.out.println("You have entered an invalid request number, try this again, please.\n");
         }
-
     }
 
-    /**
-     * Employee logout
-     * closes/ends employeeUI and returns to main screen menu
-     * saves state of employee
-     * @author DMF
-     */
     @Override
     public void closeUI() {
         employee.setE_LoggedIn(false);
@@ -246,6 +192,6 @@ public class EmployeeUI implements EmployeeUI_Interface{
         employeeScreenActive = false;
         System.out.println("\n"+employee.getE_FirstName()+", you are now logged out of {insert hotel name here} employee system\n\n");
         System.out.println("------<>----End Employee Page----<>------\n\n\n ---<--<@   Main Menu   @>-->---\n");
-
     }
+
 }
--- src/main/java/edu/ithaca/bhamula1/hotel/EmployeeUI_Interface.java
+++ src/main/java/edu/ithaca/bhamula1/hotel/EmployeeUI_Interface.java
@@ -4,14 +4,65 @@ public interface EmployeeUI_Interface {
 
 
     String getEmplTitle();
-    void uiInteraction(HotelInterface h);
-    void checkEmployeeLogIn(String el, String epwd);
-    void employeeLogScreenUI();
-    void viewOpenRequests();
-    void takeRequests(int req);
-    boolean checkEmpReq();
-    void completeRequest(int req);
-    void closeUI();
 
+    void setEmployee(EmployeeIMPL e);
+
+    /**
+     * All UI interaction below - Employee can make choice if logged in
+     * @param h Used to access employee list and requests from Hotel object
+     */
+    void uiInteraction(HotelInterface h);
+
+    /**
+     * Validates inputted employee login and password compared to employee
+     *  list; if login is accurate, save employee state
+     * @param el    Employee login input
+     * @param epwd  Employee password input
+     * @author - DMF
+     */
+    void checkEmployeeLogIn(String el, String epwd);
+
+    /**
+     * Before employee can choose options, valid input must be entered --
+     *  this method checks for valid input and calls checkEmployeeLogIn to
+     *  verify login data
+     */
+    void employeeLogScreenUI();
+
+    /**
+     * Displays active requests made for specific room numbers using the static
+     *  list of activeRequests in Hotel; displays message if there are no
+     *  active requests
+     * @author DMF
+     */
+    void viewOpenRequests();
+
+    /**
+     * Takes an active request and assigns it to an employee and the employee
+     *  to the request, and sets employee as unavailable
+     * @param req   Request number
+     */
+    void takeRequests(int req);
+
+    /**
+     * Searches for employee's assigned requests to be fulfilled, if their ID
+     *  is associated with a request
+     * @return  True if employee has request to fulfill, false otherwise
+     */
+    boolean checkEmpReq();
+
+    /**
+     * Fulfills a request associated with the employee and removes it from
+     *  list of active requests
+     * @param req   Request to be fulfilled
+     */
+    void completeRequest(int req);
+
+    /**
+     * Logs out employee; closes/ends employeeUI and returns to the main screen
+     *  menu, saving state of employee
+     * @author DMF
+     */
+    void closeUI();
 
 }
