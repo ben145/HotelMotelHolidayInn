fbed6142b844cbb67591479696d00aa8057d951e
Ben Hamula <bhamula1@ithaca.edu>
2018-05-09 10:14:16 -0400

    Javadocs

--- src/main/java/edu/ithaca/bhamula1/hotel/InventoryInterface.java
+++ src/main/java/edu/ithaca/bhamula1/hotel/InventoryInterface.java
@@ -1,14 +1,19 @@
 package edu.ithaca.bhamula1.hotel;
 
 public interface InventoryInterface {
-    //getters
     String getItem();
+
+    void setItem(String itemName);
+
     int getQuantity();
 
-    //setters
-    void setItem(String itemName);
     void setQuantity(int quantity);
 
+    /**
+     * Takes in input for item to be added to inventory, namely the name of
+     *  the item and how many/much of it is to be added
+     */
     public void addItemToInventory();
+
     public String toString_Inventory();
 }
